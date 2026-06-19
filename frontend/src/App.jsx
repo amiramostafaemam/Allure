969669777eaa4b59a32609ec388ea3b3a9a18e24
Amira Mostafa Emam <amiraaemaam62@gmail.com>
2026-06-19 21:51:52 +0300

    feat: update dependencies and integrate Sentry for error tracking
    - Added new dependencies: @sentry/react, @tailwindcss/vite, @tanstack/react-query, lucide-react, react-router, and daisyui.
    - Removed App.css and replaced it with Tailwind CSS for styling.
    - Implemented a loading state in App.jsx using PageLoader component.
    - Created new components: Footer, Layout, Navbar, PageLoader, SentryErrorFallback, and SentryUserSync.
    - Integrated Sentry for error tracking and user context synchronization with Clerk.
    - Updated main.jsx to include QueryClientProvider and Sentry error boundary.
    - Removed unused assets: hero.png, react.svg, and vite.svg.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton,  } from '@clerk/react'
-
+import { Show, SignInButton, SignUpButton, useAuth, UserButton,  } from '@clerk/react'
+import PageLoader from "./components/PageLoader"
+import Layout from './components/Layout'
 function App() {
+  const {isLoaded}=useAuth()
+
+  if(!isLoaded) return <PageLoader/>
 
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
@@ -14,7 +17,10 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
+      <p className="text-amber-400 font-bold font-stretch-75%">hhello there</p>
+      <button className='btn btn-primary'>clickk me</button>
+      <button className='btn btn-secondary'>ckkk</button>
+    </Layout>
   )
 }
 
