e0786ea3db0fe15473635840f5f6d74b74b6a67d
Amira Mostafa Emam <amiraaemaam62@gmail.com>
2026-07-14 08:44:27 +0300

    feat: implement product catalog and home page components with routing and error handling

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -27,7 +27,7 @@ export const products = pgTable("products", {
   category: text("category").notNull().default("General"),
   description: text("description").notNull().default(""),
   pricePounds: integer("price_pounds").notNull(),
-  currency: text("currency").notNull().default("usd"),
+  currency: text("currency").notNull().default("egp"),
   imageUrl: text("image_url"),
   /** ImageKit `fileId` for deletes */
   imageKitFileId: text("image_kit_file_id"),
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton,  } from '@clerk/react'
 import PageLoader from "./components/PageLoader"
 import Layout from './components/Layout'
+import { Route, Routes } from 'react-router'
+
+import HomePage from './pages/HomePage'
 function App() {
   const {isLoaded}=useAuth()
 
@@ -8,18 +11,9 @@ function App() {
 
   return (
     <Layout>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-      <p className="text-amber-400 font-bold font-stretch-75%">hhello there</p>
-      <button className='btn btn-primary'>clickk me</button>
-      <button className='btn btn-secondary'>ckkk</button>
+     <Routes>
+      <Route path='/' element={<HomePage/>}/>
+     </Routes>
     </Layout>
   )
 }
